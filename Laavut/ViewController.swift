6eca3a03a374d06ac033cbf76164b7d7c8ab62f0
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2016-07-20 14:51:13 +0300

    Locate user and center map

--- Laavut/ViewController.swift
+++ Laavut/ViewController.swift
@@ -7,19 +7,77 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    let locationManager = CLLocationManager()
+    private var mapChangedFromUserInteraction = false
+
+    @IBOutlet var mapView: MKMapView!
+
+    //MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+//            locationManager.requestLocation()
+        }
+
+        mapView.delegate = self
+        mapView.mapType = .Standard
+        mapView.zoomEnabled = true
+        mapView.scrollEnabled = true
+
+        if let coor = mapView.userLocation.location?.coordinate{
+            mapView.setCenterCoordinate(coor, animated: false)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        mapView.showsUserLocation = true
     }
 
+    override func viewWillDisappear(animated: Bool) {
+        mapView.showsUserLocation = false
+    }
 
+    //MARK: - Map
+
+    private func mapViewRegionDidChangeFromUserInteraction() -> Bool {
+        let view = self.mapView.subviews[0]
+        //  Look through gesture recognizers to determine whether this region change is from user interaction
+        if let gestureRecognizers = view.gestureRecognizers {
+            for recognizer in gestureRecognizers {
+                if( recognizer.state == UIGestureRecognizerState.Began || recognizer.state == UIGestureRecognizerState.Ended ) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    func mapView(mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        mapChangedFromUserInteraction = mapViewRegionDidChangeFromUserInteraction()
+        if (mapChangedFromUserInteraction) {
+            locationManager.stopUpdatingLocation()
+        }
+    }
+
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
+            self.mapView.setRegion(region, animated: false)
+        }
+    }
 }
-
